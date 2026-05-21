46947dafb1627a3d1a91fb4206058b7a3192d9bb
K Reethika Reddy <reddyreethika105@gmail.com>
2026-05-21 17:29:09 +0530

    add gunicorn

--- MedicineTracker_Backend/config.py
+++ MedicineTracker_Backend/config.py
@@ -1,6 +1,8 @@
+import os
+
 class Config:
-    SECRET_KEY = 'medicine_tracker_secret_2026'
-    SQLALCHEMY_DATABASE_URI = (
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'medicine_tracker_secret_2026')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 
         'mysql+pymysql://admin:Medicine1234!@medicine-tracker-db.cdwg0usiq6jf.ap-south-1.rds.amazonaws.com/medicine_tracker'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
