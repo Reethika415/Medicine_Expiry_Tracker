ea407d18846a70335e3073c2abb0e66183971ff4
K Reethika Reddy <reddyreethika105@gmail.com>
2026-05-23 17:27:24 +0530

    migrate to filess.io free MySQL

--- MedicineTracker_Backend/config.py
+++ MedicineTracker_Backend/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'medicine_tracker_secret_2026')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
-        'mysql+pymysql://admin:Medicine1234!@medicine-tracker-db.cdwg0usiq6jf.ap-south-1.rds.amazonaws.com/medicine_tracker'
+        'mysql+pymysql://medicine_tracker_betweenhit:1844c5f4adeba18ae046aba080576eeda068aa93@g9vzfc.h.filess.io:3306/medicine_tracker_betweenhit'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
